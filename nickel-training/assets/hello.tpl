0c4b2fecca93a1166506d16d30917e4ed014c228
Tomy <kazushige.tominaga@mkt-apps.com>
2017-02-05 08:52:33 +0000

    routing

---
+++ nickel-training/assets/hello.tpl
@@ -0,0 +1,15 @@
+<html> 
+  <head> 
+  <title>A Simple Mustache Demo</title> 
+  <meta charset="utf-8">
+</head> 
+<body> 
+  <h1>A Simple Mustache Demo</h1>
+    <h4>Product Info: {{name}}</h4>
+    <ul>
+      <li>Product: {{name}}</li>
+      <li>Color: {{color}}</li>
+      <li>Price: ${{price}}</li>
+    </ul>
+</body> 
+</html> 
--- nickel-training/src/main.rs
+++ nickel-training/src/main.rs
@@ -1,14 +1,17 @@
 #[macro_use] extern crate nickel;
 
-use nickel::Nickel;
+use std::collections::HashMap;
+use nickel::{Nickel, HttpRouter};
 
 fn main() {
     let mut server = Nickel::new();
 
-    server.utilize(router! {
-        get "**" => |_req, _res| {
-            "Hello world!"
-        }
+    server.get("/template", middleware! {|_req, _res|
+        let mut data = HashMap::new();
+        data.insert("color", "Green");
+        data.insert("name", "Carifornia Apple");
+        data.insert("price", "2.50");
+        return _res.render("assets/hello.tpl", &data);
     });
 
     server.listen("192.168.33.10:2000");
