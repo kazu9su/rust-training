54288fbaabeff22e841f80f9ac4d8e260aecf741
Tomy <kazushige.tominaga@mkt-apps.com>
2017-02-05 08:36:04 +0000

    hello world

---
+++ nickel-training/src/main.rs
@@ -0,0 +1,15 @@
+#[macro_use] extern crate nickel;
+
+use nickel::Nickel;
+
+fn main() {
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            "Hello world!"
+        }
+    });
+
+    server.listen("192.168.33.10:2000");
+}
